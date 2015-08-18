b2d407b4b554f8522062eff31615b6761dd8f59a
Alyssa Horrocks & Kendra Ash <student@epicodus.com>
2015-08-18 14:24:28 -0700

    Allow the ability to list out all doctors available.

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -4,10 +4,31 @@ class Doctor
 
   define_method(:initialize) do |attributes|
     @doc_name = attributes.fetch(:doc_name)
+    @specialty = attributes.fetch(:specialty)
+    @doc_id = attributes.fetch(:doc_id)
   end
 
   define_method(:doc_name) do
     @doc_name
   end
 
+  define_method(:specialty) do
+    @specialty
+  end
+
+  define_method(:doc_id) do
+    @doc_id
+  end
+
+  define_singleton_method(:all) do
+    returned_doctors = DB.exec("SELECT * FROM doctors;")
+    doctors = []
+    returned_doctors.each() do |doctor|
+      doc_name = doctors.fetch("doc_name")
+      specialty = doctors.fetch("specialty")
+      doc_id = doctors.fetch("doc_id").to_i()
+      doctors.push(Doctors.new({:doc_name => doc_name, :specialty => specialty, :doc_id => doc_id}))
+    end
+    doctors
+  end
 end
--- spec/doctor_spec.rb
+++ spec/doctor_spec.rb
@@ -12,9 +12,18 @@ end
 
 describe(Doctor) do
   describe('#doc_name') do
-    it('return the doctors name') do
+    it('return the doctors name, specialty and doc_id') do
       test_doc = Doctor.new({:doc_name => 'Benson', :specialty => 'Pediatrician', :doc_id => nil})
       expect(test_doc.doc_name()).to(eq('Benson'))
+      expect(test_doc.specialty()).to(eq('Pediatrician'))
+      # expect(test_doc.doc_id()).to(be_instance_of(Fixnum))
+    end
+  end
+
+  describe('.all') do
+    it('return an empty array at first') do
+      expect(Doctor.all()).to(eq([]))
+
     end
   end
 
