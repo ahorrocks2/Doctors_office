af71ead7e835a7cf8a60a239ee4a938ef68d9f55
Alyssa Horrocks & Kendra Ash <student@epicodus.com>
2015-08-18 16:50:44 -0700

    WOrking on ability to list patients.

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ get('/doctor/form') do
   erb(:doctor_form)
 end
 
-get('/patient/form/:id') do
+get('/patient/form/:doc_id') do
     @doctor = Doctor.find(params.fetch("doc_id").to_i())
     erb(:patient_form)
 end
@@ -31,4 +31,16 @@ post('/doctor/new') do
   doctor = Doctor.new({:doc_name => doc_name, :specialty => specialty, :doc_id => nil})
   doctor.save()
   redirect('/doctors')
+end
+
+
+post('/patient/new') do
+  @doctor = Doctor.find(params.fetch("doc_id").to_i())
+  patient_first_name = params.fetch("new_patientF")
+  patient_last_name = params.fetch("new_patientL")
+  patient_birthdate = params.fetch("new_patientBD")
+  doc_id = params.fetch("doc_id")
+  patient = Patient.new({:patient_first_name => patient_first_name, :patient_last_name => patient_last_name, :patient_birthdate => patient_birthdate, :patient_id => nil, :doc_id => doc_id})
+  patient.save()
+  redirect('/doctor/' + @doctor.doc_id().to_s())
 end
